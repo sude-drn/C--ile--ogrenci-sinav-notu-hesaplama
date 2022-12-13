fae12b02b6865e0c220caa3ad5ef06fd152ed287
sude-drn <elifsudeduram@gmail.com>
2022-12-13 12:43:12 +0300

    ögrenci sınav notu uygulaması

---
+++ öğrenci sınav notları uygulaması/öğrenci sınav notları uygulaması/Form1.cs
@@ -0,0 +1,28 @@
+namespace öğrenci_sınav_notları_uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad, soyad;
+            int s1, s2, proje;
+            double ortalama;
+            ad = textBox1.Text;
+            soyad = textBox2.Text;
+            s1 = Convert.ToInt16(textBox3.Text);
+            s2 = Convert.ToInt16(textBox4.Text);
+            proje = Convert.ToInt16(textBox5.Text);
+            ortalama = (s1 + s2+proje) / 3;
+            listBox1.Items.Add(ad + " " + soyad + " ortalama: " + ortalama);
+
+
+
+
+        }
+    }
+}
